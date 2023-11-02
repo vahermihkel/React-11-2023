cdb43d48186121843e95fb348ad78e2bc8d07c93
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-11-02 12:04:33 +0200

    reacti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="" />
+      <button className="nupu-stiil">Vajuta mind</button>
+      {/* <div>asdasd</div>
+      <p>asdsadsd</p>
+      <h1>asdasdas</h1>
+      <h2>asdasdasdas</h2> */}
+    </div>
+  );
+}
+
+export default App;
