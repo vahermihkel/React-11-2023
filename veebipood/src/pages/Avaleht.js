a139df708c696874d1abdbb2cc256cb0dc4b35c4
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-11-09 15:59:19 +0200

    firebase, useState, useRef

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,21 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // div ja span on tühjused, aga div teeb uue rea, span ei tee
 
+// tumesinine - JavaScriptis sissekirjutatud sõnad: function, const, false, true, undefined
+//  Liigitus     HTMLs elementide tagi nimetused: div, img, button, input, label
+// tavaline sinine - muutujad JS + HTMLs. Need on meie poolt välja mõeldud sõnad
+// helesinine - omadus JS + HTMLs. className, src, alt, onClick, disabled
+//              JavaScriptis: .current.value
+// kollane - funktsioonid, JS + HTMLs
+// roheline - kommentaar
+// lilla - käsklus. import, export, return, if / else
+// valge - märgid, HTMLs väljakuvatav tekst
+// oranž - tekst
+// heleroheline - number
+// {{{{{{{{{{{{{{[[[(((())))]]]}}}}}}}}}}}}}}
+
+
 function Avaleht() {
+  const [kogus, uuendaKogus] = useState(7);
+  const [laigitud, muudaLaigitud] = useState(false); // 0 - mittelaigitud, 1 - laigitud
+  const [sonum, uuendaSonum] = useState("Uuenda kogust!");
+
+  // const kogus = 7
+  // kogus = 8 xxxxxxxxxx
+
+  // kasOnDisabled = kogus === 0;
+  // document.getElementById("nupp").disabled = kasOnDisabled
+
+  // function nulli() { // Echmascript5 ja madalamal (JavaScripti erinevad versioonid)
+  // }
+
+  const nulli = () => { // Echmascript6
+    uuendaKogus(0);
+    uuendaSonum("Nullitud!");
+  }
+
+  const vahenda = () => {
+    uuendaKogus( kogus - 1  );
+    uuendaSonum("Vähendatud!");
+  }
+
+  const suurenda = () => {
+    uuendaKogus( kogus + 1  );
+    uuendaSonum("Suurendatud!");
+  }
+
   return (
     <div>
-      <img src="/mittelaigitud.svg" alt="" />
-      <img src="/laigitud.svg" alt="" />
-      <img src="/kuldne.svg" alt="" />
-      <button>Pane laigituks</button>
-      <button>Pane mittelaigituks</button>
+      <div>{sonum}</div>
+      {/* <span>{laigitud}</span> */}
+      {laigitud === false && <img src="/mittelaigitud.svg" alt="" />}
+      {laigitud === true && <img src="/laigitud.svg" alt="" />}
+      {kogus > 10 && <img src="/kuldne.svg" alt="" />}
+      <button onClick={() => muudaLaigitud(true)}>Pane laigituks</button>
+      <button onClick={() => muudaLaigitud(false)}>Pane mittelaigituks</button>
 
       <br /><br />
-
-      <button>-</button>
-      <span>7</span>
-      <button>+</button>
+      {kogus !== 0 && <button onClick={nulli}>Tagasi nulli</button>}
+      <br />
+      <button disabled={kogus === 0} onClick={vahenda}>-</button>
+      <span>{kogus}</span>
+      <button onClick={suurenda}>+</button>
     </div>
   )
 }
--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function LisaToode() {
+  const [sonum, m22raSonum] = useState("Lisa toode!");
+  const inputiLuger = useRef();
+
+  function lisa() {
+    if (inputiLuger.current.value === "") {
+      m22raSonum("Tühja nimetusega ei saa toodet lisada!");
+    } else {
+      m22raSonum("Toode lisatud: " + inputiLuger.current.value);
+    }
+  }
+
   return (
     <div>
+      <div>{sonum}</div>
       <label>Uus toode</label> <br />
-      <input type="text" /> <br />
-      <button>Sisesta</button> <br />
+      <input ref={inputiLuger} type="text" /> <br />
+      <button onClick={lisa}>Sisesta</button> <br />
+      {/* <button onClick={() => lisa()}>Sisesta</button> <br /> */}
     </div>
   )
 }
